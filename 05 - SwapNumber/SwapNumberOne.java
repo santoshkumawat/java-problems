94fad668e617e969ea45b2ef2ced036e1587252e
santosheupheus <santoshkumawat@eupheus.in>
2024-05-12 21:22:48 +0530

    05 - SwapNumbers

---
+++ 05 - SwapNumber/SwapNumberOne.java
@@ -0,0 +1,13 @@
+public class SwapNumberOne {
+    public static void main(String[] args) {
+        int a = 5, b = 10, c;
+
+        c = a;
+        a = b;
+        b = c;
+        
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+        
+    }    
+}
